45252a1429b16aae6759bb82f805c2ad3c30c1d7
steveologist <steven.camarillo.569@my.csun.edu>
2024-03-13 13:27:28 -0700

    Update script.py

--- script.py
+++ script.py
@@ -1,7 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 
-r = requests.get('http://books.toscrape.com/index.html')
-soup = BeautifulSoup(r.text, features="html.parser")
-print(soup.text)
+r = requests.get('http://books.toscrape.com/catalogue/scott-pilgrims-precious-little-life-scott-pilgrim-1_987/index.html')
+response = requests.get(r)
+soup = BeautifulSoup(response.text, "html.parser")
+
+# content_inner > article > div.row > div.col-sm-6.product_main > p.price_color
+# content_inner > article > p
+price_elements = soup.select("p.price_color")
+
+for price_element in price_elements:
+    price = price_element.get_text()
+
+print(price_elements)
+print(price)
 print(BeautifulSoup)
