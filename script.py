0ea2ba4e8e83b3c34eae7537e73065c035374def
steveologist <steven.camarillo.569@my.csun.edu>
2024-03-13 14:06:27 -0700

    Update script.py

--- script.py
+++ script.py
@@ -6,12 +6,9 @@ def scrape_product_page(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
 
-# content_inner > article > div.row > div.col-sm-6.product_main > p.price_color
-# content_inner > article > p
-price_elements = soup.select("p.price_color")
-
-for price_element in price_elements:
-    price = price_element.get_text()
-
-
-print(price)
+    # div.col-sm-6.product_main > p.price_color
+    # content_inner > article > p
+    price_elements = soup.select("p.price_color")
+    for price_element in price_elements:
+        # Do something with price_element
+        pass  # Placeholder, replace with actual processing code
