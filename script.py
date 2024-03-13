3deff511bd9e57b2b185bc9861f3e471e01b133f
steveologist <steven.camarillo.569@my.csun.edu>
2024-03-13 13:12:38 -0700

    Update script.py

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
-# import requests
+import requests
 from bs4 import BeautifulSoup
 
-# r = requests.get('http://www.example.com')
-# soup = BeautifulSoup(r.text, features="html.parser")
-# print(soup.text)
+r = requests.get('http://books.toscrape.com/index.html')
+soup = BeautifulSoup(r.text, features="html.parser")
+print(soup.text)
 print(BeautifulSoup)
