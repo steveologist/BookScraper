b2884dada3b90238ae1ed77668bc5ae577e2a89a
steveologist <steven.camarillo.569@my.csun.edu>
2024-03-13 15:00:30 -0700

    updates

--- script.py
+++ script.py
@@ -1,72 +1,35 @@
 import requests
 from bs4 import BeautifulSoup
-import csv
-
-# Function to scrape data from a product page
-
-url = 'http://books.toscrape.com/index.html'
 
 
-def scrape_product_page(url):
-
-    # Send a GET request to the product page
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-
-    # Extracting required data
-    product_page_url = url
-    upc = soup.find('th', text='UPC').find_next('td').text
-    title = soup.find('h1').text.strip()
-    p_in_tax = soup.find('th', text='P (inc)').find_next('td').text.strip()[1:]
-    p_ex_tax = soup.find('th', text='P (exc)').find_next('td').text.strip()[1:]
-    q_available = soup.find('th', text='Avail').find_next('td').text.strip()
-    description = soup.find('meta', attrs={'name': 'description'})['content']
-    cat = soup.find('a', href=lambda href: href and 'cat' in href).text.strip()
-    review_rating = soup.find('p', class_='star-rating')['class'][1]
-    image_url = soup.find('div', class_='item active').find('img')['src']
-
-    return {
-        'product_page_url': product_page_url,
-        'universal_product_code (upc)': upc,
-        'book_title': title,
-        'p_in_tax': p_in_tax,
-        'p_ex_tax': p_ex_tax,
-        'q_available': q_available,
-        'product_description': description,
-        'cat': cat,
-        'review_rating': review_rating,
-        'image_url': image_url
-    }
+url = "http://books.toscrape.com/catalogue/scott-pilgrims-precious-little-life-scott-pilgrim-1_987/index.html" # noqa
 
 
-# Function to write data to a CSV file
+response = requests.get(url)
 
 
-def write_to_csv(data, filename):
-
-    with open(filename, 'w', newline='', encoding='utf-8') as csvfile:
-        fieldnames = data[0].keys()
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-        for row in data:
-            writer.writerow(row)
+soup = BeautifulSoup(response.text, "html.parser")
 
 
-# Main function
+book_title = soup.select_one("div.col-sm-6.product_main > h1").text.strip()
+product_page_url = url
+upc = soup.select_one("tr:nth-child(1) > td").text.strip() # noqa
+p_inc_tax = soup.select_one("tr:nth-of-type(4) > td").text.strip()
+p_exc_tax = soup.select_one("tr:nth-of-type(3) > td").text.strip()
+quantity_available = soup.select_one("tr:nth-of-type(6) > td").text.strip()
+product_description = soup.select_one("article.product_page > p").text.strip()
+category = soup.select("ul.breadcrumb > li")[2].text.strip()
+review_rating = soup.select_one("p.star-rating")['class'][1]
+image_url = soup.select_one("div.item.active > img")['src']
 
 
-def main():
-
-    # URL of the product page
-    url = 'http://books.toscrape.com/index.html'
-
-    # Scrape data from the product page
-
-    product_data = scrape_product_page(url)
-
-    # Write data to a CSV file
-    write_to_csv([product_data], 'product_data.csv')
-
-
-if __name__ == '__main__':
-    main()
+print("Book Title:", book_title)
+print("Product Page URL:", product_page_url)
+print("Universal Product Code (UPC):", upc)
+print("Price Including Tax:", p_inc_tax)
+print("Price Excluding Tax:", p_exc_tax)
+print("Quantity Available:", quantity_available)
+print("Product Description:", product_description)
+print("Category:", category)
+print("Review Rating:", review_rating)
+print("Image URL:", image_url)
