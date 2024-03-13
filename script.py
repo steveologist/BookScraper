6620dcd52a5f3cb256250662d8fc35d14074c7ef
steveologist <steven.camarillo.569@my.csun.edu>
2024-03-13 13:32:16 -0700

    Update script.py

--- script.py
+++ script.py
@@ -7,7 +7,7 @@ soup = BeautifulSoup(response.text, "html.parser")
 
 # content_inner > article > div.row > div.col-sm-6.product_main > p.price_color
 # content_inner > article > p
-price_elements = soup.select("p.price_color")
+price_elements = soup.select(".price_color")
 
 for price_element in price_elements:
     price = price_element.get_text()
