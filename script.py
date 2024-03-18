e3600dd42e6d191aad117db95791f2f87b9cbaad
steveologist <steven.camarillo.569@my.csun.edu>
2024-03-17 17:41:01 -0700

    updates

--- script.py
+++ script.py
@@ -3,63 +3,37 @@ from bs4 import BeautifulSoup
 import csv
 
 
-url = "http://books.toscrape.com/catalogue/scott-pilgrims-precious-little-life-scott-pilgrim-1_987/index.html" # noqa
+def scrape_book_data(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    product_page_url = url
+    book_title = soup.select_one("div.col-sm-6.product_main > h1").text.strip()
+    upc = soup.select_one("tr:nth-child(1) > td").text.strip()
+    p_inc_tax = soup.select_one("tr:nth-of-type(4) > td").text.strip()
+    p_exc_tax = soup.select_one("tr:nth-of-type(3) > td").text.strip()
+    quantity_available = soup.select_one("tr:nth-of-type(6) > td").text.strip() # noqa
+    product_description = soup.select_one("article.product_page > p").text.strip() # noqa
+    category = soup.select("ul.breadcrumb > li")[2].text.strip()
+    review_rating = soup.select_one("p.star-rating")['class'][1]
+    image_url = soup.select_one("div.item.active > img")['src']
+
+    row = [product_page_url, book_title, upc, p_inc_tax, p_exc_tax, quantity_available, product_description, category, review_rating, image_url] # noqa
+    return row
 
 
-response = requests.get(url)
+def write_to_csv(filename, rows):
+    with open(filename, 'w', newline='', encoding='utf-8') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['Product Page URL', 'Book Title', 'UPC', 'Price Including Tax', 'Price Excluding Tax', 'Quantity Available', 'Product Description', 'Category', 'Review Rating', 'Image URL']) # noqa
+        writer.writerow(rows)
 
 
-soup = BeautifulSoup(response.text, "html.parser")
+# thru stackoverflow this conditional block allows You to Execute Code When the File Runs as a Script # noqa
+if __name__ == "__main__":
+    url = "http://books.toscrape.com/catalogue/scott-pilgrims-precious-little-life-scott-pilgrim-1_987/index.html" # noqa
+    row = scrape_book_data(url)
+    write_to_csv('.csv', row)
+    print("Data extracted into .csv")
 
-
-product_page_url = url
-book_title = soup.select_one("div.col-sm-6.product_main > h1").text.strip()
-upc = soup.select_one("tr:nth-child(1) > td").text.strip() # noqa
-p_inc_tax = soup.select_one("tr:nth-of-type(4) > td").text.strip()
-p_exc_tax = soup.select_one("tr:nth-of-type(3) > td").text.strip()
-quantity_available = soup.select_one("tr:nth-of-type(6) > td").text.strip()
-product_description = soup.select_one("article.product_page > p").text.strip()
-category = soup.select("ul.breadcrumb > li")[2].text.strip()
-review_rating = soup.select_one("p.star-rating")['class'][1]
-image_url = soup.select_one("div.item.active > img")['src']
-
-
-cat_url = "http://books.toscrape.com/catalogue/category/books/horror_31/index.html" # noqa
-
-response = requests.get(cat_url)
-
-soup = BeautifulSoup(response.text, "html.parser")
-
-# iterates over each of the selected <a> elements found in the previous step and extracts the value of the href attribute from each anchor tag.  # noqa
-# The href attribute contains the URL to which the link points.
-book_urls = [a['href'] for a in soup.select('h3 a')]
-
-cat_books = []
-for cat_url in cat_urls
-
-
-# CREATING ROWS FOR CSV FILES
-row = [product_page_url, book_title, upc, p_inc_tax, p_exc_tax, quantity_available, product_description, category, review_rating, image_url]  # noqa 
-# header = ['Product Page URL', 'Book Title', 'UPC', 'Price Including Tax', 'Price Excluding Tax', 'Quantity Available', 'Product Description', 'Category', 'Review Rating', 'Image URL'] # noqa
-
-# using Write to External Files method
-# use the  .writer()  and  .writerow()  functions to determine columns into a CSV file. # noqa
-with open('.csv', 'w', newline='', encoding='utf-8') as csvfile:
-    writer = csv.writer(csvfile)
-    writer.writerow(['Product Page URL', 'Book Title', 'UPC', 'Price Including Tax', 'Price Excluding Tax', 'Quantity Available', 'Product Description', 'Category', 'Review Rating', 'Image URL']) # noqa
-    writer.writerow(row)
-
-print("hope this works")
-
-# HERE I MADE THE CODE PRINT DIRECTLY ON TERMINAL
-# CODE WORKS AND SCRAPES FROM WEBSIRE
-# print("Product Page URL:", product_page_url)
-# print("Book Title:", book_title)
-# print("Universal Product Code (UPC):", upc)
-# print("Price Including Tax:", p_inc_tax)
-# print("Price Excluding Tax:", p_exc_tax)
-# print("Quantity Available:", quantity_available)
-# print("Product Description:", product_description)
-# print("Category:", category)
-# print("Review Rating:", review_rating)
-# print("Image URL:", image_url)
+    print("did this work????")
