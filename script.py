0f1db023eb7c4275d90201e2cb77d8ab27e97aab
steveologist <steven.camarillo.569@my.csun.edu>
2024-03-20 11:12:44 -0700

    Update script.py

--- script.py
+++ script.py
@@ -3,56 +3,46 @@ from bs4 import BeautifulSoup
 import csv
 
 
-def scrape_book_data(url):
+def scrape_book_info(url):
     response = requests.get(url)
-    soup = BeautifulSoup(response.text, "html.parser")
+    soup = BeautifulSoup(response.content, 'html.parser')
 
     product_page_url = url
-    book_title = soup.select_one("div.col-sm-6.product_main > h1").text.strip()
-    upc = soup.select_one("tr:nth-child(1) > td").text.strip() # noqa
-    p_inc_tax = soup.select_one("tr:nth-of-type(4) > td").text.strip()
-    p_exc_tax = soup.select_one("tr:nth-of-type(3) > td").text.strip()
-    quantity_available = soup.select_one("tr:nth-of-type(6) > td").text.strip()
-    product_description = soup.select_one("article.product_page > p").text.strip() # noqa
-    category = soup.select("ul.breadcrumb > li")[2].text.strip()
-    review_rating = soup.select_one("p.star-rating")['class'][1]
-    image_url = soup.select_one("div.item.active > img")['src']
+    upc = soup.find('td', text='UPC').find_next_sibling('td').text
+    book_title = soup.find('h1').text
+    price_including_tax = soup.find('td', text='Price (incl. tax)').find_next_sibling('td').text # noqa
+    price_excluding_tax = soup.find('td', text='Price (excl. tax)').find_next_sibling('td').text # noqa
+    quantity_available = soup.find('td', text='Availability').find_next_sibling('td').text # noqa
+    product_description = soup.find('meta', attrs={'name': 'description'})['content'] # noqa
+    category = soup.find('ul', class_='breadcrumb').find_all('li')[2].text.strip() # noqa
+    review_rating = soup.find('p', class_='star-rating')['class'][1]
+    image_url = soup.find('img')['src']
+
+    return {
+        'product_page_url': product_page_url,
+        'universal_product_code (upc)': upc,
+        'book_title': book_title,
+        'price_including_tax': price_including_tax,
+        'price_excluding_tax': price_excluding_tax,
+        'quantity_available': quantity_available,
+        'product_description': product_description,
+        'category': category,
+        'review_rating': review_rating,
+        'image_url': image_url
+    }
 
 
-# CREATING ROWS FOR CSV FILES
-    row = [product_page_url, book_title, upc, p_inc_tax, p_exc_tax, quantity_available, product_description, category, review_rating, image_url]  # noqa 
-    return row
-# header = ['Product Page URL', 'Book Title', 'UPC', 'Price Including Tax', 'Price Excluding Tax', 'Quantity Available', 'Product Description', 'Category', 'Review Rating', 'Image URL'] # noqa
+def write_to_csv(data, filename):
+    with open(filename, 'w', newline='', encoding='utf-8') as file:
+        writer = csv.DictWriter(file, fieldnames=data[0].keys())
+        writer.writeheader()
+        writer.writerows(data)
 
 
-def extract_product_urls(url):
-    product_urls = []
-    while url:
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        products = soup.find_all('h3')
-        for product in products:
-            product_url = product.find('a')['href']
-            product_urls.append('http://books.toscrape.com/catalogue/' + product_url) # noqa
-        next_page = soup.find('li', class_='next')
-        if next_page:
-            url = 'http://books.toscrape.com/catalogue/category/books/horror_31/' + next_page.find('a')['href'] # noqa
-        else:
-            url = None
-    return product_urls
+def main():
+    book_info = scrape_book_info("http://books.toscrape.com/catalogue/scott-pilgrims-precious-little-life-scott-pilgrim-1_987/index.html") # noqa
+    write_to_csv([book_info], 'book_info.csv')
 
 
 if __name__ == "__main__":
-    url = "http://books.toscrape.com/catalogue/scott-pilgrims-precious-little-life-scott-pilgrim-1_987/index.html" # noqa
-    row = scrape_book_data(url)
-    base_url = 'http://books.toscrape.com/catalogue/category/books/horror_31/index.html' # noqa
-    product_urls = extract_product_urls(base_url)
-
-# using Write to External Files method
-# use the  .writer()  and  .writerow()  functions to determine columns into a CSV file. # noqa
-with open('.csv', 'w', newline='', encoding='utf-8') as csvfile:
-    writer = csv.writer(csvfile)
-    writer.writerow(['Product Page URL', 'Book Title', 'UPC', 'Price Including Tax', 'Price Excluding Tax', 'Quantity Available', 'Product Description', 'Category', 'Review Rating', 'Image URL']) # noqa
-    writer.writerow(row)
-
-print("hope this works")
+    main()
