33f2ca0673a53d3417d5f796de3b73f6b5330b4e
steveologist <steven.camarillo.569@my.csun.edu>
2024-03-20 13:49:08 -0700

    UPDATES
    string-switch / return clean up

--- script.py
+++ script.py
@@ -1,48 +1,38 @@
+# phase 1 complete
+
 import requests
 from bs4 import BeautifulSoup
 import csv
 
 
-def scrape_book_info(url):
+def get_book_info(url):
     response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
+    soup = BeautifulSoup(response.text, 'html.parser')
 
+    # Extracting information
     product_page_url = url
-    upc = soup.find('td', text='UPC').find_next_sibling('td').text
-    book_title = soup.find('h1').text
-    price_including_tax = soup.find('td', text='Price (incl. tax)').find_next_sibling('td').text # noqa
-    price_excluding_tax = soup.find('td', text='Price (excl. tax)').find_next_sibling('td').text # noqa
-    quantity_available = soup.find('td', text='Availability').find_next_sibling('td').text # noqa
-    product_description = soup.find('meta', attrs={'name': 'description'})['content'] # noqa
+    upc = soup.find('th', string='UPC').find_next('td').text # noqa
+    book_title = soup.find('div', class_='col-sm-6 product_main').find('h1').text # noqa
+    price_including_tax = soup.find('th', string='Price (incl. tax)').find_next('td').text # noqa
+    price_excluding_tax = soup.find('th', string='Price (excl. tax)').find_next('td').text # noqa
+    quantity_available = soup.find('th', string='Availability').find_next('td').text # noqa
+    product_description = soup.find('div', id='product_description').find_next('p').text # noqa
     category = soup.find('ul', class_='breadcrumb').find_all('li')[2].text.strip() # noqa
     review_rating = soup.find('p', class_='star-rating')['class'][1]
-    image_url = soup.find('img')['src']
+    image_url = soup.find('div', class_='item active').find('img')['src']
 
-    return {
-        'product_page_url': product_page_url,
-        'universal_product_code (upc)': upc,
-        'book_title': book_title,
-        'price_including_tax': price_including_tax,
-        'price_excluding_tax': price_excluding_tax,
-        'quantity_available': quantity_available,
-        'product_description': product_description,
-        'category': category,
-        'review_rating': review_rating,
-        'image_url': image_url
-    }
+    return [product_page_url, upc, book_title, price_including_tax, price_excluding_tax, quantity_available, product_description, category, review_rating, image_url] # noqa
 
 
-def write_to_csv(data, filename):
-    with open(filename, 'w', newline='', encoding='utf-8') as file:
-        writer = csv.DictWriter(file, fieldnames=data[0].keys())
-        writer.writeheader()
+def write_to_csv(data):
+    headers = ["product_page_url", "universal_product_code (upc)", "book_title", "price_including_tax", "price_excluding_tax", "quantity_available", "product_description", "category", "review_rating", "image_url"] # noqa
+    with open('.csv', 'w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow(headers)
         writer.writerows(data)
 
 
-def main():
-    book_info = scrape_book_info("http://books.toscrape.com/catalogue/scott-pilgrims-precious-little-life-scott-pilgrim-1_987/index.html") # noqa
-    write_to_csv([book_info], 'book_info.csv')
-
-
 if __name__ == "__main__":
-    main()
+    url = "http://books.toscrape.com/catalogue/scott-pilgrims-precious-little-life-scott-pilgrim-1_987/index.html" # noqa
+    book_info = get_book_info(url)
+    write_to_csv([book_info])
